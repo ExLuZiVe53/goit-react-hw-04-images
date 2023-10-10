6880ceef04a48736f0759a0f7118a0425aa3442c
ExLuZiVe53 <imstudent27@gmail.com>
2023-10-10 18:30:25 +0200

    replays in hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Loader from './Loader/Loader.jsx';
 import { Notify } from 'notiflix';
@@ -8,22 +8,27 @@ import { ImageGallery } from './ImageGallery/ImageGallery.jsx';
 import { SearchBar } from './SearchBar/Searchbar.jsx';
 import { ErMessage } from './SearchBar/ErMessage.jsx';
 
-export class App extends Component {
+const App = () => {
   // Stay abortControl
+  
   abortCtrl;
-  // Стан
-  state = {
-    isLoading: false,
-    error: null,
-    searchAr: [],
-    searchImg: '',
-    page: 1,
-    isShow: false,
-  };
+
+  // Стан, переписуємо його за допомогою хуків useState
+
+  const [isLoading, setIsloading] = useState(false);
+  const [error, setError] = useState(null);
+  const [searchAr, setSearchAr] = useState([]);
+  const [searchImg, setSearchImg] = useState('');
+  const [page, setPage] = useState(1);
+  const [isShow, setIsShow] = useState(false);
+
+  
 
   // write Submit function
   handleSabmit = input => {
-    this.setState({ searchImg: input, page: 1 });
+    setSearchImg(input)
+    setPage(1)
+    
   };
   //Styles with APP
   appStyle = {
@@ -35,31 +40,34 @@ export class App extends Component {
     color: '#010101',
   };
   // Життєвий цикл
-  async componentDidUpdate(_, nextState) {
+   useEffect((prevProps, nextState)=> {
+ async
     // перевірка на однаковий ввід та повтор сторінок запиту
     if (
-      this.state.searchImg === nextState.searchImg &&
-      this.state.page === nextState.page
+      searchImg === nextState.searchImg &&
+      page === nextState.page
     ) {
       return;
     }
 
     // перевірка на новий пошук в інпуті
-    if (this.state.searchImg !== nextState.searchImg) {
-      this.setState({
-        searchAr: [],
-        page: 1,
-        isShow: false,
-      });
+    if (searchImg !== nextState.searchImg) {
+      setSearchAr([])
+      setPage(1)
+      setIsShow(false)
+      
     }
     try {
-      const { searchImg, page } = this.state;
+      // const { searchImg, page } = this.state;
+
       //ініціалізація абортконтролера
-      this.abortCtrl = new AbortController();
+      abortCtrl = new AbortController();
       // зміна стану
-      this.setState({ isLoading: true, error: null });
+      
+      setIsloading(true)
+      setError(null)
       //запит на API
-      const images = await fetchPictures(searchImg, this.abortCtrl, page);
+      const images =  await fetchPictures(searchImg, abortCtrl, page);
       // Нотифікашка скільки є картинок по запиту
       if (images.totalhits) {
         Notify.info(`Hooray! We found ${images.totalHits} images.`);
@@ -67,7 +75,7 @@ export class App extends Component {
       // додаю у стан масив даних для галереї
       console.log(images.hits);
 
-      this.setState(
+      setIsShow(
         prevImages => ({
           searchAr: [
             ...prevImages.searchAr,
@@ -91,38 +99,37 @@ export class App extends Component {
       );
       //перевірка на останю партію картинок і приховання кнопки LoadMore
       if (images.hits.length < 12) {
-        this.setState({
-          isShow: false,
-        });
+        setIsShow(false)
         Notify.failure('Sorry, that is all results.');
       }
     } catch (error) {
       if (error.code !== 'ERR_CANCELED') {
-        this.setState({
-          error: 'Somethink was wrong! Please reloading the page.',
-        });
+        setError('Somethink was wrong! Please reloading the page.')
+        
       }
     } finally {
-      this.setState({ isLoading: false });
+      setIsloading(false)
     }
   }
+  },[])
+  
 
   //новий запит по кліку на LoadMore
-  newFetchImages = () => {
-    this.setState({
-      page: this.state.page + 1,
-    });
+
+  const newFetchImages = () => {
+    setPage(page + 1);
   };
 
-  render() {
+   {
     // Деструкторизуємо значення в стейті
-    const { isLoading, searchAr, isShow, error } = this.state;
+
+    // const { isLoading, searchAr, isShow, error } = this.state;
 
     return (
-      <div style={this.appStyle}>
+      <div style={appStyle}>
         <SearchBar
-          handleSabmit={this.handleSabmit}
-          handleChange={this.handleChange}
+          handleSabmit={handleSabmit}
+          handleChange={handleChange}
         />
         {isLoading && (
           // true
@@ -130,8 +137,148 @@ export class App extends Component {
         )}
         {error && <ErMessage>{error}</ErMessage>}
         <ImageGallery images={searchAr} />
-        {isShow && <BTNLoadMore onChange={this.newFetchImages} />}
+        {isShow && <BTNLoadMore onChange={newFetchImages} />}
       </div>
     );
   }
-}
+
+
+export default App;
+
+// import React, { Component } from 'react';
+
+// import Loader from './Loader/Loader.jsx';
+// import { Notify } from 'notiflix';
+// import { BTNLoadMore } from './Button/Button.jsx';
+// import { fetchPictures } from './Api/fetchPictures.js';
+// import { ImageGallery } from './ImageGallery/ImageGallery.jsx';
+// import { SearchBar } from './SearchBar/Searchbar.jsx';
+// import { ErMessage } from './SearchBar/ErMessage.jsx';
+
+// export class App extends Component {
+//   // Stay abortControl
+//   abortCtrl;
+//   // Стан
+//   state = {
+//     isLoading: false,
+//     error: null,
+//     searchAr: [],
+//     searchImg: '',
+//     page: 1,
+//     isShow: false,
+//   };
+
+//   // write Submit function
+//   handleSabmit = input => {
+//     this.setState({ searchImg: input, page: 1 });
+//   };
+//   //Styles with APP
+//   appStyle = {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     justifyContent: 'center',
+//     alignItems: 'stretch',
+//     fontSize: 40,
+//     color: '#010101',
+//   };
+//   // Життєвий цикл
+//   async componentDidUpdate(_, nextState) {
+//     // перевірка на однаковий ввід та повтор сторінок запиту
+//     if (
+//       this.state.searchImg === nextState.searchImg &&
+//       this.state.page === nextState.page
+//     ) {
+//       return;
+//     }
+
+//     // перевірка на новий пошук в інпуті
+//     if (this.state.searchImg !== nextState.searchImg) {
+//       this.setState({
+//         searchAr: [],
+//         page: 1,
+//         isShow: false,
+//       });
+//     }
+//     try {
+//       const { searchImg, page } = this.state;
+//       //ініціалізація абортконтролера
+//       this.abortCtrl = new AbortController();
+//       // зміна стану
+//       this.setState({ isLoading: true, error: null });
+//       //запит на API
+//       const images = await fetchPictures(searchImg, this.abortCtrl, page);
+//       // Нотифікашка скільки є картинок по запиту
+//       if (images.totalhits) {
+//         Notify.info(`Hooray! We found ${images.totalHits} images.`);
+//       }
+//       // додаю у стан масив даних для галереї
+//       console.log(images.hits);
+
+//       this.setState(
+//         prevImages => ({
+//           searchAr: [
+//             ...prevImages.searchAr,
+//             ...images.hits.map(({ tags, id, largeImageURL, webformatURL }) => ({
+//               tags,
+//               id,
+//               largeImageURL,
+//               webformatURL,
+//             })),
+//           ],
+//           isShow: true,
+//         }),
+//         //плавний скрол
+//         () => {
+//           if (page !== 1)
+//             window.scrollBy({
+//               top: 260 * 3,
+//               behavior: 'smooth',
+//             });
+//         }
+//       );
+//       //перевірка на останю партію картинок і приховання кнопки LoadMore
+//       if (images.hits.length < 12) {
+//         this.setState({
+//           isShow: false,
+//         });
+//         Notify.failure('Sorry, that is all results.');
+//       }
+//     } catch (error) {
+//       if (error.code !== 'ERR_CANCELED') {
+//         this.setState({
+//           error: 'Somethink was wrong! Please reloading the page.',
+//         });
+//       }
+//     } finally {
+//       this.setState({ isLoading: false });
+//     }
+//   }
+
+//   //новий запит по кліку на LoadMore
+//   newFetchImages = () => {
+//     this.setState({
+//       page: this.state.page + 1,
+//     });
+//   };
+
+//   render() {
+//     // Деструкторизуємо значення в стейті
+//     const { isLoading, searchAr, isShow, error } = this.state;
+
+//     return (
+//       <div style={this.appStyle}>
+//         <SearchBar
+//           handleSabmit={this.handleSabmit}
+//           handleChange={this.handleChange}
+//         />
+//         {isLoading && (
+//           // true
+//           <Loader />
+//         )}
+//         {error && <ErMessage>{error}</ErMessage>}
+//         <ImageGallery images={searchAr} />
+//         {isShow && <BTNLoadMore onChange={this.newFetchImages} />}
+//       </div>
+//     );
+//   }
+// }
