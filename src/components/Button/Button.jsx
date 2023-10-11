f3aa4995db7c5e061557fda9dbb515e4161f5f08
ExLuZiVe53 <imstudent27@gmail.com>
2023-10-11 20:23:25 +0200

    reload component Button with hooks

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,11 @@
 import { Button } from './Button.styled';
 
-export const BTNLoadMore = ({ onChange }) => {
-  return <Button onClick={() => onChange()}>Load More</Button>;
+export const BTNLoadMore = ({ newFetchImages }) => {
+  return <Button onClick={() => newFetchImages()}>Load More</Button>;
 };
+
+// import { Button } from './Button.styled';
+
+// export const BTNLoadMore = ({ onChange }) => {
+//   return <Button onClick={() => onChange()}>Load More</Button>;
+// };
