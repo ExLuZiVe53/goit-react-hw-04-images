e9e7feaa21c5f9b3aaa6ddbc6661b180e295be10
ExLuZiVe53 <imstudent27@gmail.com>
2023-10-11 20:23:51 +0200

    reload component ImageGalery with hooks

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,44 +1,85 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem.jsx';
 import { ImaGalleryUl } from './ImageGallery.styled.js';
 import { Modal } from '../Modal/Modal.jsx';
 
-export class ImageGallery extends Component {
-  //стан
-  state = {
-    ImageData: {},
-    isModal: false,
-  };
+const ImageGallery = ({ images }) => {
+  const [isModal, setIsShowModal] = useState(false);
+  const [ImageData, setImageData] = useState({});
   //знаходжу картинку по кліку по айді
-  getImgById = id =>
-    this.setState({
-      ImageData: this.props.images.find(el => el.id === id),
-      isModal: true,
-    });
-  //закриття модалки
-  modalIsClose = () => {
-    this.setState({ isModal: false });
+  const getImgById = id => {
+    setIsShowModal(true);
+    setImageData(images.find(el => el.id === id));
   };
 
-  render() {
-    return (
-      <ImaGalleryUl>
-        {this.props.images.map(img => {
-          return (
-            <ImageGalleryItem
-              key={img.id}
-              isModal={this.state.isModal}
-              getImgById={this.getImgById}
-              picture={img}
-            />
-          );
-        })}
-        <Modal
-          modalIsOpen={this.state.isModal}
-          img={this.state.ImageData}
-          closeModal={this.modalIsClose}
-        ></Modal>
-      </ImaGalleryUl>
-    );
+  //   //закриття модалки
+  function modalIsClose() {
+    setIsShowModal(false);
   }
-}
+
+  return (
+    <ImaGalleryUl>
+      {images.map(img => {
+        return (
+          <ImageGalleryItem
+            key={img.id}
+            isModal={isModal}
+            getImgById={getImgById}
+            picture={img}
+          />
+        );
+      })}
+      <Modal
+        modalIsOpen={isModal}
+        img={ImageData}
+        closeModal={modalIsClose}
+      ></Modal>
+    </ImaGalleryUl>
+  );
+};
+export default ImageGallery;
+
+// import { Component } from 'react';
+// import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem.jsx';
+// import { ImaGalleryUl } from './ImageGallery.styled.js';
+// import { Modal } from '../Modal/Modal.jsx';
+
+// export class ImageGallery extends Component {
+//   //стан
+//   state = {
+//     ImageData: {},
+//     isModal: false,
+//   };
+//   //знаходжу картинку по кліку по айді
+//   getImgById = id =>
+//     this.setState({
+//       ImageData: this.props.images.find(el => el.id === id),
+//       isModal: true,
+//     });
+//   //закриття модалки
+//   modalIsClose = () => {
+//     this.setState({ isModal: false });
+//   };
+
+//   render() {
+//     return (
+//       <ImaGalleryUl>
+//         {this.props.images.map(img => {
+//           return (
+//             <ImageGalleryItem
+//               key={img.id}
+//               isModal={this.state.isModal}
+//               getImgById={this.getImgById}
+//               picture={img}
+//             />
+//           );
+//         })}
+//         <Modal
+//           modalIsOpen={this.state.isModal}
+//           img={this.state.ImageData}
+//           closeModal={this.modalIsClose}
+//         ></Modal>
+//       </ImaGalleryUl>
+//     );
+//   }
+// }
