4e6291d74fa1aac22444847fff2545446ab051b1
ExLuZiVe53 <imstudent27@gmail.com>
2023-10-11 20:24:13 +0200

    reload component Modal with hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -25,8 +25,6 @@ const customStyles = {
 };
 ReactModal.setAppElement('#root');
 export const Modal = ({ img, closeModal, modalIsOpen }) => {
-
-
   return (
     <ReactModal
       isOpen={modalIsOpen}
@@ -37,9 +35,52 @@ export const Modal = ({ img, closeModal, modalIsOpen }) => {
       style={customStyles}
       contentLabel="Example Modal"
     >
-      
       <img src={img.largeImageURL} alt={img.tags} loading="lazy" />
-      
     </ReactModal>
   );
 };
+
+// import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+// import ReactModal from 'react-modal';
+// const customStyles = {
+//   overlay: {
+//     position: 'fixed',
+//     top: 0,
+//     left: 0,
+//     width: '100vw',
+//     height: '100vh',
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: 'rgba(18, 17, 17, 0.8)',
+//     zIndex: 1200,
+//   },
+//   content: {
+//     maxWidth: 'calc(100vw - 48px)',
+//     maxHeight: 'calc(100vh - 24px)',
+//     padding: 10,
+//     border: 'none',
+//     position: 'static',
+//     borderRadius: 0,
+//     overflow: 'hidden',
+//   },
+// };
+// ReactModal.setAppElement('#root');
+// export const Modal = ({ img, closeModal, modalIsOpen }) => {
+
+//   return (
+//     <ReactModal
+//       isOpen={modalIsOpen}
+//       shouldCloseOnOverlayClick={true}
+//       onAfterOpen={() => disableBodyScroll(document)}
+//       onAfterClose={() => enableBodyScroll(document)}
+//       onRequestClose={() => closeModal()}
+//       style={customStyles}
+//       contentLabel="Example Modal"
+//     >
+
+//       <img src={img.largeImageURL} alt={img.tags} loading="lazy" />
+
+//     </ReactModal>
+//   );
+// };
